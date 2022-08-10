00e3f442fedfcfb7bd7cd37c42160d4b270493ff
agrosimi <michael.simmler@agroscope.admin.ch>
2022-08-10 15:57:29 +0200

    autoformat load_methods.R

--- R/000_load_methods.R
+++ R/000_load_methods.R
@@ -8,19 +8,28 @@
 ### --> here work needs to be done; helpful message should be raised in case of problems
 
 determine_sampInt <- function(tbl) {
+   sInt_by_id <-
+      tbl[, .(sInt = unique(difftime(time[-1], time[-length(time)], units = "secs"))), by = id]
 
-  sInt_by_id <- tbl[, .(sInt = unique(difftime(time[-1], time[-length(time)], units = "secs"))), by = id]
+   freq_grid = 1 / (1:1000)
 
-  freq_grid = 1 / c(1:1000)
+   round_to_freq_interv <-
+      function(x) {
+         sapply(x, \(x) freq_grid[which.min(abs(freq_grid - x))])
+      }
 
-  round_to_freq_interv <- function(x) {sapply(x, function(x) freq_grid[which.min(abs(freq_grid - x))])}
+   sInt <- unique(sInt_by_id[, round_to_freq_interv(sInt)])
 
-  sInt <- unique(sInt_by_id[, round_to_freq_interv(sInt)])
+   checkmate::assertNumber(
+      sInt,
+      finite = TRUE,
+      lower = 0,
+      null.ok = FALSE,
+      na.ok = FALSE,
+      .var.name = "PROBLEM WITH YOUR SAMPLING FREQ - Maebe you use data with differrent freqs? Or your cow id is not unique?..."
+   )
 
-  checkmate::assertNumber(sInt, finite = TRUE, lower = 0, null.ok = FALSE,  na.ok = FALSE,
-                          .var.name = "PROBLEM WITH YOUR SAMPLING FREQ - Maebe you use data with differrent freqs? Or your cow id is not unique?...")
-
-  return(as.difftime(sInt, units = "secs"))
+   return(as.difftime(sInt, units = "secs"))
 
 }
 
@@ -28,59 +37,82 @@ determine_sampInt <- function(tbl) {
 
 # source: https://adv-r.hadley.nz/conditions.html
 stop_custom <- function(.subclass, message, call = NULL, ...) {
-   err <- structure(
-      list(
-         message = message,
-         call = call,
-         ...
-      ),
-      class = c(.subclass, "error", "condition")
-   )
+   err <- structure(list(message = message,
+                         call = call,
+                         ...),
+                    class = c(.subclass, "error", "condition"))
    stop(err)
 }
 
 # ----------------------------------------------------------------
 
 load_files <- function(input,
-                      id_substring,
-                      timeFwdUpRight_cols = c(1, 2, 3, 4),
-                      time_format  = NULL,
-                      tz           = Sys.timezone(),
-                      start_time   = NULL,
-                      end_time     = NULL,
-                      sep          = "auto",
-                      skip         = "__auto__",
-                      parallel     = 1,
-                      ...) {
-
+                       id_substring,
+                       timeFwdUpRight_cols = c(1, 2, 3, 4),
+                       time_format  = NULL,
+                       tz           = Sys.timezone(),
+                       start_time   = NULL,
+                       end_time     = NULL,
+                       sep          = "auto",
+                       skip         = "__auto__",
+                       parallel     = 1,
+                       ...) {
    # argument checks
 
    assertColl <- checkmate::makeAssertCollection()
 
    if (checkmate::testDirectory(input[1])) {
-      checkmate::assertDirectory(input, access = "r", add = assertColl)
+      checkmate::assertDirectory(input,
+                                 access = "r",
+                                 add = assertColl)
    } else if (checkmate::testFile(input[1])) {
-      checkmate::assertFile(input, access = "r", extension = c("csv", "tsv", "txt"), add = assertColl)
+      checkmate::assertFile(
+         input,
+         access = "r",
+         extension = c("csv", "tsv", "txt"),
+         add = assertColl
+      )
    } else {
       assertColl$push("'input' not found, must be directory or file names. See ?Triact")
    }
 
    if (checkmate::testNumeric(id_substring)) {
-      msg <- checkmate::checkIntegerish(id_substring, len = 2, sorted = TRUE,
-                                        any.missing = FALSE, lower = 1)
+      msg <- checkmate::checkIntegerish(
+         id_substring,
+         len = 2,
+         sorted = TRUE,
+         any.missing = FALSE,
+         lower = 1
+      )
       if (!isTRUE(msg)) {
-         assertColl$push(paste0("Variable 'id_substring' as integer c(first, last): ", msg))
+         assertColl$push(paste0(
+            "Variable 'id_substring' as integer c(first, last): ",
+            msg
+         ))
       }
    } else if (checkmate::testCharacter(id_substring)) {
-      msg <- checkmate::checkCharacter(id_substring, len = 1, min.chars = 1, all.missing = FALSE)
+      msg <- checkmate::checkCharacter(
+         id_substring,
+         len = 1,
+         min.chars = 1,
+         all.missing = FALSE
+      )
       if (!isTRUE(msg)) {
-         assertColl$push(paste0("Variable 'id_substring' as character (Perl-like regex): ", msg))
+         assertColl$push(paste0(
+            "Variable 'id_substring' as character (Perl-like regex): ",
+            msg
+         ))
       }
    } else {
-      "'id_substring' misspecified. See ?Triact"
+      assertColl$push("'id_substring' misspecified. See ?Triact")
    }
 
-   msg <- checkmate::checkIntegerish(timeFwdUpRight_cols, len = 4, lower = 1, all.missing = FALSE)
+   msg <- checkmate::checkIntegerish(
+      timeFwdUpRight_cols,
+      len = 4,
+      lower = 1,
+      all.missing = FALSE
+   )
 
    if (!isTRUE(msg)) {
       assertColl$push(paste0("Variable 'timeFwdUpRight_cols': ", msg))
@@ -101,9 +133,14 @@ load_files <- function(input,
 
    # extracts ID from file names
    if (is.character(id_substring)) {
-      names(input) <- sapply(regmatches(basename(input), regexec(id_substring, basename(input), perl = TRUE)), function(i) i[1])
+      names(input) <-
+         sapply(regmatches(
+            basename(input),
+            regexec(id_substring, basename(input), perl = TRUE)
+         ), \(i) i[1])
    } else if (is.numeric(id_substring)) {
-      names(input) <- substring(basename(input), id_substring[1], id_substring[2])
+      names(input) <-
+         substring(basename(input), id_substring[1], id_substring[2])
    }
 
    # extract math sign of accel cols to new var and strip it from timeFwdUpRight_cols
@@ -113,7 +150,8 @@ load_files <- function(input,
    timeFwdUpRight_cols <- abs(timeFwdUpRight_cols)
 
    # prepare col names
-   colnms <- c("time", "acc_fwd", "acc_up", "acc_right")[!is.na(timeFwdUpRight_cols)]
+   colnms <-
+      c("time", "acc_fwd", "acc_up", "acc_right")[!is.na(timeFwdUpRight_cols)]
 
    # prepare col classes: when user does not supply time_format then POSIXct for time col, otherwise character
    colcls <- as.list(na.omit(timeFwdUpRight_cols))
@@ -125,49 +163,79 @@ load_files <- function(input,
 
    # -----------------------------------------------------------
 
-   arguments <- c(list(
-                  select = timeFwdUpRight_cols,
-                  tz = if (tz == "UTC") "UTC" else "", # fread only takes "UTC" or "" (system tz) --> extra step below needed
-                  col.names = colnms,
-                  colClasses = colcls,
-                  sep = sep,
-                  skip = skip,
-                  header = FALSE), list(...))
+   read_file <- function(f, fread_args, tformat = time_format) {
+      file_dt <- do.call(data.table::fread, c(list(file = f), fread_args))
 
-   read_file <- function(f, arguments, tfo = time_format) {
+      if (!is.null(tformat)) {
+         file_dt[, time := lubridate::parse_date_time(
+            time,
+            orders = tformat,
+            tz = tz,
+            exact = TRUE,
+            quiet = TRUE
+         )]
+      }
 
-         file_dt <- do.call(data.table::fread, c(list(file = f), arguments))
-
-         if (!is.null(tfo)) {
-            file_dt[, time := lubridate::parse_date_time(time, orders = tfo,
-                                                         tz = tz, exact = TRUE, quiet = TRUE)]
-         }
-
-         if ((!lubridate::is.POSIXct(file_dt$time)) || (sum(is.na(file_dt$time)) > 0)) {
-            stop_custom("time_parse_error", basename(f))
-         }
+      if ((!lubridate::is.POSIXct(file_dt$time)) ||
+          (sum(is.na(file_dt$time)) > 0)) {
+         stop_custom("time_parse_error", basename(f))
+      }
       return(file_dt)
    }
+
+   arguments <- c(
+      list(
+         select = timeFwdUpRight_cols,
+         tz = if (tz == "UTC")
+            "UTC"
+         else
+            "",
+         # fread only takes "UTC" or "" (system tz) --> extra step below needed
+         col.names = colnms,
+         colClasses = colcls,
+         sep = sep,
+         skip = skip,
+         header = FALSE
+      ),
+      list(...)
+   )
 
    # Note on parallelization: when parallel > 1 files are read in parallel via parLapply
    # in this case nThread in data.table::fread is set to zero to avoid nested parallelization
    # this behavior can be overwritten by passing nTread via the ... argument
 
    if (parallel > 1) {
-      if (is.null(arguments$nThread)) arguments$nThread <- 1 # to avoid nested parallelization
+      if (is.null(arguments$nThread)) {
+         # to avoid nested parallelization
+         arguments$nThread <- 1
+      }
       fread_cls <- parallel::makeCluster(parallel)
       on.exit(parallel::stopCluster(fread_cls))
-      dataList <- parallel::parLapply(cl = fread_cls, input,
-                                            function(f) tryCatch(read_file(f, arguments, time_format),
-                                                                 time_parse_error = identity))
+      dataList <- parallel::parLapply(cl = fread_cls,
+                                      X = input,
+                                      fun = \(f) {
+                                         tryCatch(
+                                            expr = read_file(f, arguments),
+                                            time_parse_error = identity
+                                         )
+                                      })
    } else {
-      dataList <- lapply(input, function(f) tryCatch(read_file(f, arguments, time_format),
-                                                     time_parse_error = identity))
+      dataList <- lapply(X = input,
+                         FUN = \(f) tryCatch(
+                            expr = read_file(f, arguments),
+                            time_parse_error = identity
+                         ))
    }
 
-   tErrMsg <- unlist(dataList[vapply(dataList, is, logical(1), "time_parse_error")], use.names = FALSE)
+   tErrMsg <- unlist(dataList[vapply(dataList,
+                                     FUN = is,
+                                     FUN.VALUE = logical(1),
+                                     class2 = "time_parse_error")])
    if (!is.null(tErrMsg)) {
-      stop(paste0("Problem coersing time column to POSIXct for files: ", paste(tErrMsg, collapse = ", ")))
+      stop(paste0(
+         "Problem coersing time column to POSIXct for files: ",
+         paste(tErrMsg, collapse = ", ")
+      ))
    }
    private$dataDT <- data.table::rbindlist(dataList, idcol = "id")
 
@@ -176,8 +244,10 @@ load_files <- function(input,
    # according to the math sign specified via the timeFwdUpRight_cols argument
 
    expr <- str2lang(paste0("list(" ,
-                           paste(ifelse(acc_col_sign == -1, paste0("-", colnms[-1]), colnms[-1]),
-                                 collapse = ", "),
+                           paste(
+                              ifelse(acc_col_sign == -1, paste0("-", colnms[-1]), colnms[-1]),
+                              collapse = ", "
+                           ),
                            ")"))
 
    private$dataDT[, colnms[-1] := eval(expr)]
@@ -185,19 +255,24 @@ load_files <- function(input,
    # ---------------------------
    private$dataDT[, id := as.factor(id)]
 
-   private$dataDT <- private$dataDT[complete.cases(private$dataDT), ]
+   private$dataDT <-
+      private$dataDT[complete.cases(private$dataDT),]
 
-   private$dataDT <- private$dataDT[!duplicated(private$dataDT), ]
+   private$dataDT <- private$dataDT[!duplicated(private$dataDT),]
 
    attr(private$dataDT$time, "tzone") <- tz
 
    # filter time range according to user-provided start and/or end time
    if (!is.null(start_time) && is.null(start_time)) {
-      private$dataDT <- private$dataDT[time >= as.POSIXct(start_time, tz = tz), ]
+      private$dataDT <-
+         private$dataDT[time >= as.POSIXct(start_time, tz = tz),]
    } else if (is.null(start_time) && !is.null(end_time)) {
-      private$dataDT <- private$dataDT[time <= as.POSIXct(end_time, tz = tz)]
+      private$dataDT <-
+         private$dataDT[time <= as.POSIXct(end_time, tz = tz)]
    } else if (!is.null(start_time) && !is.null(end_time)) {
-      private$dataDT <- private$dataDT[(time >= as.POSIXct(start_time, tz = tz)) & (time <= as.POSIXct(end_time, tz = tz))]
+      private$dataDT <-
+         private$dataDT[(time >= as.POSIXct(start_time, tz = tz)) &
+                           (time <= as.POSIXct(end_time, tz = tz))]
    }
 
    # --------------------------
@@ -218,18 +293,14 @@ load_files <- function(input,
 # ----------------------------------------------------------------
 
 load_table <- function(table) {
+   private$dataDT <- as.data.table(table)
+   private$sampInt <- determine_sampInt(private$dataDT)
 
-  private$dataDT <- as.data.table(table)
-  private$sampInt <- determine_sampInt(private$dataDT)
-
-  # note availability of acceleration directions (for checks by other methods)
-  private$has_data <- checkmate::checkDataTable(private$dataDT, min.rows = 1)
-  private$has_fwd <- "acc_fwd" %in% colnames(private$dataDT)
-  private$has_up <- "acc_up" %in% colnames(private$dataDT)
-  private$has_right <- "acc_right" %in% colnames(private$dataDT)
+   # note availability of acceleration directions (for checks by other methods)
+   private$has_data <-
+      checkmate::checkDataTable(private$dataDT, min.rows = 1)
+   private$has_fwd <- "acc_fwd" %in% colnames(private$dataDT)
+   private$has_up <- "acc_up" %in% colnames(private$dataDT)
+   private$has_right <- "acc_right" %in% colnames(private$dataDT)
 
 }
-
-
-
-
