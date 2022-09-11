6a818be86aca5674397c9d8c4f48a0f525f9f633
agrosimi <michael.simmler@agroscope.admin.ch>
2022-09-11 11:27:29 +0200

    tidy add methods

--- R/002_add_methods.R
+++ R/002_add_methods.R
@@ -2,38 +2,116 @@
 # add_... methods of Triact class
 # ----------------------------------------------------------------
 
+################################################################################
+
 add_lying <- function(filter_method = "median",
-                             crit_lie = 0.5,
-                             minimum_duration_lying = 30,
-                             minimum_duration_standing = NULL,
-                             add_filtered = FALSE,
-                             ...) {
+                      crit_lie = 0.5,
+                      minimum_duration_lying = 30,
+                      minimum_duration_standing = NULL,
+                      add_filtered = FALSE,
+                      ...) {
+
+  # check prerequisites --------------------------------------------------------
 
   checkmate::assertTRUE(private$has_data, .var.name = "has data?")
   checkmate::assertTRUE(private$has_up, .var.name = "has upward acceleration?")
-  checkmate::assertNumber(crit_lie)
-  #checkmate::assertNumber(cutoff, lower = 0, finite = TRUE)
 
-  # list filter method's default values
-
-  filterArgs_defaults <- list()
-
-  filterArgs_defaults[["median"]] = list(window_size = 10)
-
-  filterArgs_defaults[["butter"]] = list(cutoff = 0.1,
-                                         order = 1)
-
-  # get args for filter method passed via ... and complete with default values
+  # collect "..." arguments to be past to filter method ------------------------
+  # and complete with default values
 
   fArgs <- list(...)
 
-  for (a in names(filterArgs_defaults[[filter_method]])) {
-    if (is.null(fArgs[[a]])) {
-      fArgs[a] <- filterArgs_defaults[[filter_method]][a]
+  fArgsDef <- list()
+
+  if (filter_method == "median") {
+
+    fArgsDef <- list(window_size = 10) # Defaults for "median"
+
+  } else if (filter_method == "butter") {
+
+    fArgsDef <- list(cutoff = 0.1, # Defaults for "butter"
+                     order = 1)
+  }
+
+  fArgs <- c(fArgs, fArgsDef[!names(fArgsDef) %in% names(fArgs)])
+
+  # argument checks ------------------------------------------------------------
+
+  assertColl <- checkmate::makeAssertCollection()
+
+  # ---- check filter_method ----
+  checkmate::assertChoice(filter_method,
+                          choices = c("median", "butter"),
+                          add = assertColl)
+
+  # ---- check crit_lie ----
+  checkmate::assertNumber(crit_lie,
+                          add = assertColl)
+
+  # ---- check minimum_duration_lying ----
+  checkmate::assertNumber(minimum_duration_lying,
+                          lower = 0,
+                          null.ok = TRUE,
+                          add = assertColl)
+
+  # ---- check minimum_duration_standing ----
+  checkmate::assertNumber(minimum_duration_standing,
+                          lower = 0,
+                          null.ok = TRUE,
+                          add = assertColl)
+
+  # ---- check add_filtered ----
+  checkmate::assertFlag(add_filtered,
+                        add = assertColl)
+
+  # ---- check "..." arguments ----
+
+  if (filter_method == "median" && (length(fArgs) > 0)) {
+
+    checkmate::assertNames(names(fArgs),
+                           type = "unique",
+                           subset.of = names(fArgsDef),
+                           add = assertColl,
+                           what = "arguments for filter_method 'median'",
+                           .var.name = "...")
+
+    checkmate::assertNumber(fArgs$window_size,
+                            lower = 0,
+                            add = assertColl,
+                            .var.name = "window_size")
+
+
+  } else if (filter_method == "butter" && (length(fArgs) > 0)) {
+
+    checkmate::assertNames(names(fArgs),
+                           type = "unique",
+                           subset.of = names(fArgsDef),
+                           add = assertColl,
+                           what = "arguments for filter_method 'butter'",
+                           .var.name = "...")
+
+    if ("cutoff" %in% names(fArgs)) {
+      checkmate::assertNumber(fArgs$cutoff,
+                              lower = 0,
+                              # Nyquist freq
+                              upper = 0.5 * 1 / as.numeric(private$sampInt,
+                                                            units = "secs"),
+                              add = assertColl,
+                              .var.name = "cutoff")
+    }
+
+    if ("order" %in% names(fArgs)) {
+      checkmate::assertInt(fArgs$order,
+                           lower = 1,
+                           add = assertColl,
+                           .var.name = "order")
     }
   }
 
-  print(fArgs)
+  checkmate::reportAssertions(assertColl)
+
+
+  # determine lying/standing  and bouts ----------------------------------------
 
   ## Step 1: filtering signal
 
@@ -92,7 +170,7 @@ add_lying <- function(filter_method = "median",
   private$dataDT[, bout_nr := cumsum(c(1, diff(lying) != 0)), id]
 
 
-  ## tidy, update
+  # Tidy and update ------------------------------------------------------------
 
   # Order columns with lying information
   co <- c("bout_nr", "lying", "acc_up_filtered")
@@ -117,19 +195,37 @@ add_lying <- function(filter_method = "median",
 
   }
 
-# ----------------------------------------------------------------
+################################################################################
 
-add_side <- function(left_leg, crit_left = if(left_leg) -0.5 else 0.5){
+add_side <- function(left_leg, crit_left = if(left_leg) -0.5 else 0.5) {
+
+  # check prerequisites --------------------------------------------------------
+
   checkmate::assertTRUE(private$has_data, .var.name = "has data?")
   checkmate::assertTRUE(private$has_lying, .var.name = "lying added?")
   checkmate::assertTRUE(private$has_right, .var.name = "has right-axis acceleration?")
-  if (missing(crit_left)) {checkmate::assertFlag(left_leg)}
-  checkmate::assertNumber(crit_left)
-  if (!missing(crit_left) & !missing(left_leg)) {
-    warning("The argument 'left_leg' is ignored as argument 'crit_left' was provided.", call. = FALSE)
+
+  # argument checks ------------------------------------------------------------
+
+  if (missing(crit_left)) {
+    checkmate::assertFlag(left_leg)
   }
 
-  private$dataDT[, side := as.factor(if(!lying[1]) NA else if (median(acc_right < crit_left)) "L" else "R"), by = .(id, bout_nr)]
+  checkmate::assertNumber(crit_left)
+
+  if (!missing(crit_left) & !missing(left_leg)) {
+    warning("The argument 'left_leg' is ignored as
+            argument 'crit_left' was provided.", call. = FALSE)
+  }
+
+  # determine lying side -------------------------------------------------------
+
+  private$dataDT[, side := as.factor(if(!lying[1]) NA
+                                     else if (median(acc_right < crit_left)) "L"
+                                     else "R"),
+                 by = .(id, bout_nr)]
+
+  # Tidy and update ------------------------------------------------------------
 
   private$has_side <- TRUE
 
@@ -137,16 +233,27 @@ add_side <- function(left_leg, crit_left = if(left_leg) -0.5 else 0.5){
 
 }
 
-# ----------------------------------------------------------------
+################################################################################
 
 add_activity <- function(add_jerk = FALSE) {
+
+  # check prerequisites --------------------------------------------------------
+
   checkmate::assertTRUE(private$has_data, .var.name = "has data?")
 
-  private$dataDT[, delta_time := as.numeric(c(NA, difftime(time[-1], time[-length(time)], units = "secs"))), by = id]
+  # argument checks ------------------------------------------------------------
+
+  checkmate::assertFlag(add_jerk)
+
+  # determine activity  --------------------------------------------------------
+
+  private$dataDT[, delta_time := as.numeric(
+    c(NA, difftime(time[-1], time[-length(time)], units = "secs"))), by = id]
 
   axis <- c(private$has_fwd, private$has_up, private$has_right)
 
-  private$dataDT[, c("jerk_fwd", "jerk_up", "jerk_right")[axis] := lapply(.SD, \(x) {c(NA, diff(x)) / delta_time}),
+  private$dataDT[, c("jerk_fwd", "jerk_up", "jerk_right")[axis] :=
+                   lapply(.SD, \(x) {c(NA, diff(x)) / delta_time}),
                  .SDcols = c("acc_fwd", "acc_up", "acc_right")[axis]]
 
   private$dataDT[, delta_time := NULL]
@@ -154,11 +261,12 @@ add_activity <- function(add_jerk = FALSE) {
   private$dataDT[, activity := sqrt(rowSums(sapply(.SD, \(x) x^2))),
                  .SDcols = c("jerk_fwd", "jerk_up", "jerk_right")[axis]]
 
+  # Tidy and update ------------------------------------------------------------
+
   if (!add_jerk) {
-
     private$dataDT[, c("jerk_fwd", "jerk_up", "jerk_right")[axis] := NULL]
-
   }
+
   private$has_activity <- TRUE
 
   return(invisible(self))
