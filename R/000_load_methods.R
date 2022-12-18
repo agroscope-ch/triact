6b97ed6c2f912e398601927b06730c10a0d1e1b1
Simmler Michael <michael.simmler@agroscope.admin.ch>
2022-12-18 15:31:25 +0100

    add argument checks and error messages further down in $load_files

--- R/000_load_methods.R
+++ R/000_load_methods.R
@@ -130,16 +130,24 @@ load_files <- function(input,
     if (is.na(timeFwdUpRight_cols[1])) {
     assertColl$push("Variable 'timeFwdUpRight_cols': First element (time column) cannot be NA.")
     }
-    if (sum(is.na(timeFwdUpRight_cols[2:4])) == 3) {
+    if (checkmate::allMissing(timeFwdUpRight_cols[2:4])) {
       assertColl$push("Variable 'timeFwdUpRight_cols': At least one of the acceleration columns must be non-NA.")
     }
   }
 
   ## check time_format
+  try_err <-
+    suppressWarnings(try(lubridate::parse_date_time("_", orders = time_format),
+                         silent = TRUE))
 
-
-  ##### CHECK STILL MISSING
-
+  if (!checkmate::testCharacter(time_format,
+                                min.len = 1,
+                                null.ok = TRUE,
+                                any.missing = FALSE) ||
+      (inherits(try_err, "try-error") &&
+      grepl("Unknown formats supplied", attr(try_err, "condition")$message))) {
+    assertColl$push("Variable 'time_format': Unknown format(s) supplied.")
+  }
 
   ## check tz
   if (inherits(try(lubridate::force_tz(Sys.time(), tz), silent = TRUE),
@@ -154,29 +162,35 @@ load_files <- function(input,
           !checkmate::testPOSIXct(as.POSIXct(get(tp)),
                                   len = 1,
                                   any.missing = FALSE)) {
-        print(tp)
         assertColl$push(paste0(
           "Variable '", tp, "': Not interpretable as POSIXct of length 1"))
       }
     }
   }
 
-
   ## check sep
+  checkmate::assertCharacter(sep,
+                             len = 1,
+                             all.missing = FALSE,
+                             add = assertColl)
 
   ## check skip
+  if (!checkmate::testCharacter(skip,
+                               len = 1,
+                               all.missing = FALSE) &&
+      !checkmate::testInt(skip,
+                          na.ok = FALSE)) {
+    assertColl$push("Variable 'skip': Must be integer or character of length 1")
+  }
 
   ## check parallel
-
-
+  checkmate::assertInt(parallel,
+                       lower = 1,
+                       add = assertColl)
 
   checkmate::reportAssertions(assertColl)
 
-
-
-
-  # new title  ----------------------------------------------------------------
-
+  # prepare for reading  -------------------------------------------------------
 
   # list files if input is dir
   if (dir.exists(input[1])) {
@@ -195,17 +209,21 @@ load_files <- function(input,
       substring(basename(input), id_substring[1], id_substring[2])
   }
 
-  # extract math sign of accel cols to new var and strip it from timeFwdUpRight_cols
+  if (checkmate::anyMissing(names(input))) {
+   stop("Missspecified 'id_substring'. Could not extract id from files: ",
+        paste(basename(input[is.na(names(input))]), collapse = ", "),
+        call. = FALSE)
+  }
 
+  ## extract math sign of accel cols & strip it from timeFwdUpRight_cols
   acc_col_sign <- na.omit(sign(timeFwdUpRight_cols[-1]))
-
   timeFwdUpRight_cols <- abs(timeFwdUpRight_cols)
 
-  # prepare col names
+  ## prepare col names
   colnms <-
     c("time", "acc_fwd", "acc_up", "acc_right")[!is.na(timeFwdUpRight_cols)]
 
-  # prepare col classes: when user does not supply time_format then POSIXct for time col, otherwise character
+  ## prepare col classes
   colcls <- as.list(na.omit(timeFwdUpRight_cols))
   if (is.null(time_format)) {
     names(colcls) <- c("POSIXct", rep("numeric", length(colcls) - 1))
@@ -213,7 +231,7 @@ load_files <- function(input,
     names(colcls) <- c("character", rep("numeric", length(colcls) - 1))
   }
 
-  # -----------------------------------------------------------
+  # read files & concatenate ---------------------------------------------------
 
   read_file <- function(f, fread_args, tformat = time_format) {
     file_dt <- do.call(data.table::fread, c(list(file = f), fread_args))
@@ -229,7 +247,7 @@ load_files <- function(input,
     }
 
     if ((!lubridate::is.POSIXct(file_dt$time)) ||
-        (sum(is.na(file_dt$time)) > 0)) {
+        checkmate::anyMissing(file_dt$time)) {
       stop_custom("time_parse_error", basename(f))
     }
     return(file_dt)
@@ -252,13 +270,12 @@ load_files <- function(input,
     list(...)
   )
 
-  # Note on parallelization: when parallel > 1 files are read in parallel via parLapply
-  # in this case nThread in data.table::fread is set to zero to avoid nested parallelization
-  # this behavior can be overwritten by passing nTread via the ... argument
+  ## Note on parallelization: when parallel > 1 files are read in parallel via parLapply
+  ## in this case nThread in data.table::fread is set to zero to avoid nested parallelization
+  ## this behavior can be overwritten by passing nTread via the ... argument
 
   if (parallel > 1) {
     if (is.null(arguments$nThread)) {
-      # to avoid nested parallelization
       arguments$nThread <- 1
     }
     fread_cls <- parallel::makeCluster(parallel)
@@ -289,33 +306,39 @@ load_files <- function(input,
       paste(tErrMsg, collapse = ", ")
     ))
   }
+
   private$dataDT <- data.table::rbindlist(dataList, idcol = "id")
 
-  # ---------------------------
-  # Negation of the acceleration data (opposing direction)
-  # according to the math sign specified via the timeFwdUpRight_cols argument
+  # tidy, clean etc. -----------------------------------------------------------
+
+  ## Negation of the acceleration data (opposing direction)
+  ## according to the math sign specified via the timeFwdUpRight_cols argument
 
   expr <- str2lang(paste0("list(" ,
                           paste(
-                            ifelse(acc_col_sign == -1, paste0("-", colnms[-1]), colnms[-1]),
+                            ifelse(acc_col_sign == -1,
+                                   paste0("-", colnms[-1]), colnms[-1]),
                             collapse = ", "
                           ),
                           ")"))
 
   private$dataDT[, colnms[-1] := eval(expr)]
 
-  # ---------------------------
+  ## id to factor
   private$dataDT[, id := as.factor(id)]
 
+  ## drop incomplete
   private$dataDT <-
     private$dataDT[complete.cases(private$dataDT),]
 
+  ## drop duplicates
   private$dataDT <- private$dataDT[!duplicated(private$dataDT),]
 
+  ## set timezone
   attr(private$dataDT$time, "tzone") <- tz
 
-  # filter time range according to user-provided start and/or end time
-  if (!is.null(start_time) && is.null(start_time)) {
+  ## filter time range according to user-provided start and/or end time
+  if (!is.null(start_time) && is.null(end_time)) {
     private$dataDT <-
       private$dataDT[time >= as.POSIXct(start_time, tz = tz),]
   } else if (is.null(start_time) && !is.null(end_time)) {
@@ -327,9 +350,36 @@ load_files <- function(input,
                        (time <= as.POSIXct(end_time, tz = tz))]
   }
 
-  # --------------------------
+  ## drop unused levels in id & warn user
+
+  old_levels <- private$dataDT[, levels(id)]
+
+  private$dataDT[, id := droplevels(id)]
+
+  dropped_levels <- setdiff(old_levels, levels(private$dataDT$id))
+
+  if (length(dropped_levels) > 0) {
+    warning(paste0("Filtering using the user-provided start_time/end_time ",
+                    "resulted in no data for ids: ",
+                    paste(dropped_levels, collapse = ", ")),
+            call. = FALSE)
+  }
+
+  if (nrow(private$dataDT) == 0) {
+
+    # reset triact object if no data left
+    name_of_triact_obtect <- as.character(sys.calls()[[1]][[1]])[2]
+    assign(name_of_triact_obtect, Triact$new(), inherits = TRUE)
+
+    stop("No data after filtering with user-provided start_time/end_time left.",
+         call. = FALSE)
+  }
+
+  # determine sampling interval ------------------------------------------------
 
   private$sampInt <- determine_sampInt(private$dataDT)
+
+  # return ---------------------------------------------------------------------
 
   return(invisible(self))
 }
@@ -342,7 +392,7 @@ load_table <- function(table) {
 
   assertColl <- checkmate::makeAssertCollection()
 
-  checkmate::assertDataFrame(table)
+  checkmate::assertDataFrame(table, min.rows = 2)
 
   checkmate::assertNames(colnames(table),
                          what = "colnames",
