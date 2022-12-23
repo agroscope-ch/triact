b57c6ffc21b0091b71046616970d8c160c8b51d8
Simmler Michael <michael.simmler@agroscope.admin.ch>
2022-12-23 21:35:05 +0100

    fix error/warning for CRAN

--- vignettes/prerender_vignettes.R
+++ vignettes/prerender_vignettes.R
@@ -6,3 +6,7 @@ library(knitr)
 # Execute the code from the vignette
 
 knitr::knit("vignettes/triact.Rmd.orig", output = "vignettes/triact.Rmd")
+
+# make sure figures are in vignette (not figures) and path is correct in vignette
+
+# add rendered pdf, html to inst/doc
