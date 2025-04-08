3e1c6186173cf36a0651c92bb192d2b00560bfc4
Simmler Michael <michael.simmler@agroscope.admin.ch>
2025-04-08 16:27:27 +0200

    CRAN fix undefined global variables (data.table)

--- R/011_CRAN_fix.R
+++ R/011_CRAN_fix.R
@@ -3,7 +3,7 @@
 
 N <- acc_fwd <- acc_right <- acc_up <- boutDuration <- bout_nr <- delta_time <-
 duration <- endTime <- gravity_up <- id <- lying <- private <- propI <- side <-
-startTime <- add_lying <- self <- NULL
+startTime <- add_lying <- self <- sInt <- sInt_id_different <- sInt_inconsistent <- sInt_median <- NULL
 
 # https://stackoverflow.com/questions/43662416/when-using-data-table-in-a-package-r-cmd-check-notes-no-visible-global-functio
 "." <- list
